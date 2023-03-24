ff8f54cce05672b809f69faad7b13fd28d239405
Leandro Gonzalez <leandrogonzalezbatista@yahoo.es>
2023-03-23 23:32:17 -0400

    new network file checking

--- lib/network wireshark.py
+++ lib/network wireshark.py
@@ -1,35 +1,18 @@
-import pyshark
+from scapy.all import *
 
-# Define the path to the network traffic report file
-file_path = "./data/network.pcapng"
+# Load the pcap file
+packets = rdpcap('C:\\Users\\leand\\Documents\\Projects\\ai\\data\\mynet.pcapng')
 
-# Create a FileCapture object to read the network traffic report
-cap = pyshark.FileCapture(file_path)
+# Iterate over each packet in the pcap file
+for packet in packets:
+    # Check if the packet has any errors
+    if packet.haslayer('ICMP') and packet['ICMP'].type == 3:
+        print(f"Error: {packet['ICMP'].type} - {packet['ICMP'].code} - {packet['IP'].src} - {packet['IP'].dst}")
+    # Check if the packet is a TCP SYN scan
+    elif packet.haslayer('TCP') and packet['TCP'].flags == 'S':
+        print(f"Possible TCP SYN scan: {packet['IP'].src} - {packet['IP'].dst}")
+    # Check if the packet is a TCP XMAS scan
+    elif packet.haslayer('TCP') and packet['TCP'].flags == 'FPU':
+        print(f"Possible TCP XMAS scan: {packet['IP'].src} - {packet['IP'].dst}")
 
-# Define a flag to indicate whether any anomalies have been found
-anomaly_found = False
-
-# Loop through each packet in the network traffic report
-for packet in cap:
-    # Check for suspicious activity in the packet
-    if packet.tcp.flags_push == '1':
-        print("Suspicious activity detected in packet {}: PUSH flag set".format(packet.number))
-        anomaly_found = True
-    elif packet.tcp.flags_fin == '1' and packet.tcp.flags_ack == '0':
-        print("Suspicious activity detected in packet {}: FIN flag set without ACK".format(packet.number))
-        anomaly_found = True
-
-# Close the network traffic report file
-cap.close()
-
-# Check if any anomalies were found and print a message if necessary
-if anomaly_found:
-    print("Anomalies detected in the network traffic report")
-else:
-    print("No anomalies detected in the network traffic report")
-
-
-
-#read the file line by line
-
-
+    
